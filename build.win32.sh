0ecfef6ab3db5f4831cfa201a8c299c9e3ef25ca
AlaskanEmily <emily@alaskanemily.net>
2019-06-18 23:46:56 -0700

    Fix missing library with cygwin

--- build.win32.sh
+++ build.win32.sh
@@ -3,4 +3,4 @@
 # Any copyright is dedicated to the Public Domain.
 # http://creativecommons.org/publicdomain/zero/1.0/
 
-make BACKEND=win32 GFXLIBRARY="-lgdi32 -luser32 -lcomdlg32" ARCH=amd64 PLATFORM=elf64 DELETE=rm COMPILER=gcc COMPILERFLAGS="-c -Immu -Icpu -Igpu -Idbg -O2 -Wall -Wextra -pedantic -g -ansi -DNDEBUG" COMPILEOUT="-o " LINKER=gcc LINKFLAGS="-g" LINKOUT="-o " EXE=.exe OBJ=.o LIB=.a $*
+make BACKEND=win32 GFXLIBRARY="-lgdi32 -luser32 -lcomdlg32 -lcomctl32" ARCH=amd64 PLATFORM=elf64 DELETE=rm COMPILER=gcc COMPILERFLAGS="-c -Immu -Icpu -Igpu -Idbg -O2 -Wall -Wextra -pedantic -g -ansi -DNDEBUG" COMPILEOUT="-o " LINKER=gcc LINKFLAGS="-g" LINKOUT="-o " EXE=.exe OBJ=.o LIB=.a $*
