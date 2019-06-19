2dcbdab493b7e0e5b9dd476e4444cfa5b8b0a49a
AlaskanEmily <emily@alaskanemily.net>
2019-06-18 20:12:58 -0700

    Fix typo

--- gpu/gpu.h
+++ gpu/gpu.h
@@ -3,7 +3,9 @@
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- */#ifndef GG_GPU_GPU_H
+ */
+
+#ifndef GG_GPU_GPU_H
 #define GG_GPU_GPU_H
 #pragma once
 
